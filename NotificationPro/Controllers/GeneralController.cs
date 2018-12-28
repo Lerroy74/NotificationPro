f27aff9872ecc2173f34563ba4b63f2767ca1a16
Lerroy <Lerroy74@mail.ru>
2018-12-29 00:52:42 +0500

    initial

---
+++ NotificationPro/Controllers/GeneralController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NotificationPro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeneralController : ControllerBase
+    {
+        public IActionResult Test()
+        {
+            return Ok("Тест");
+        }
+    }
+}
